48a2747a26afe1c7ed43d6c353c24ba112959cd7
Erik Ronning <erik_ronning@alumni.brown.edu>
2025-07-24 14:22:48 -0400

    remove

--- src/components/content-block/content-block-body.tsx
+++
@@ -1,9 +0,0 @@
-import { Card } from "@chakra-ui/react";
-
-export const ContentBlockBody: React.FC<React.PropsWithChildren> = ({
-  children,
-}) => (
-  <Card.Body px={6} pt={6} pb={6} borderRadius="md">
-    {children}
-  </Card.Body>
-);
--- src/components/content-block/content-block.tsx
+++
@@ -1,5 +0,0 @@
-import { Card } from "@chakra-ui/react";
-
-export const ContentBlock: React.FC<React.PropsWithChildren> = ({
-  children,
-}) => <Card.Root>{children}</Card.Root>;
