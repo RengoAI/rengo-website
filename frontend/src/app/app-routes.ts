3ba7461cb601d12c74e1c4cee8e491b63c811c3c
Erik Ronning <erik_ronning@alumni.brown.edu>
2025-07-26 13:30:46 -0400

    chore(ENG-165): add privacy policy (#17)
    * add privacy policy page
    
    * nit
    
    * fix lint
    
    * add two new production branch lint and tests actions
    
    * make sure to include a release sha

--- frontend/src/app/app-routes.ts
+++ frontend/src/app/app-routes.ts
@@ -16,6 +16,7 @@ export const rootRoute = route(
       {},
       {
         termsOfService: route("/terms-of-service", {}, {}),
+        privacyPolicy: route("/privacy-policy", {}, {}),
       },
     ),
     notFound: route("/*", {}, {}),
